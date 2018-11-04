8180f09c9f983e15da0543b65bcb1bfb5a2314fd
maher.hamoui@gmail.com <dadycool24*>
2018-11-04 08:01:26 +0200

    products module initialize

---
+++ src/app/pages/products/products.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NewProductComponent} from './new-product/new-product.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NewProductComponent]
+})
+export class ProductsModule {
+}
