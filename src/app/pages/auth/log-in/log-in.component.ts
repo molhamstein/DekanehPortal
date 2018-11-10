3b4812866a5ab997c03128ec3dc28c26c01a55c0
maher.hamoui@gmail.com <dadycool24*>
2018-11-10 14:29:20 +0200

    prod api

--- src/app/pages/auth/log-in/log-in.component.ts
+++ src/app/pages/auth/log-in/log-in.component.ts
@@ -13,7 +13,7 @@ export class LogInComponent implements OnInit {
   ngOnInit() {
   }
   logIn(form){
-    this.http.post('http://104.217.253.15:3003/api/users/staffLogin',form.value).subscribe((data)=>{
+    this.http.post('http://167.99.214.78/api/users/staffLogin', form.value).subscribe((data) => {
       localStorage.setItem('token',JSON.parse(data['_body']).id);
       this.router.navigate(['']);
     },(err)=>{
