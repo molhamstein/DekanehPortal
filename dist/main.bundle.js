28a18236af6f5493a4d2261093814f96b5203f35
anas-1995 <anasalazmeh.95@gmail.com>
2019-04-22 19:26:27 +0300

    new portal3

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -17140,10 +17140,10 @@ AmCharts.maps.worldLow={"svg":{"defs":{"amcharts:ammap":{"projection":"mercator"
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
 var environment = {
     production: true,
-    // baseUrl: 'http://dockaan.com:3033/api'
+    baseUrl: 'http://dockaan.com:3033/api'
     // baseUrl: 'http://localhost:3000/api'
     // baseUrl: 'http://104.217.253.15:3030/api',
-    baseUrl: 'http://192.168.1.2:3033/api'
+    // baseUrl: 'http://192.168.1.2:3033/api'
 };
 //# sourceMappingURL=environment.prod.js.map
 
